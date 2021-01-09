c3bd66f9adc8e082df2851d61b6104eee912d79a
Sergey <KononenkoSergey@mail.ru>
2021-01-09 20:06:16 +0300

    1 Work with interface 2 Create logic

--- HelloWorldApp2/ViewController.swift
+++ HelloWorldApp2/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var showTextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        helloWorldLabel.isHidden = true
+        helloWorldLabel.textColor = .red
+        showTextButton.layer.cornerRadius = 10
     }
 
 
+    @IBAction func showTextButtonPressed() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            showTextButton.setTitle("Hide text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            showTextButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 }
 
